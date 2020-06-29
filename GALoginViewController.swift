0b180061b2239065f47bf604dc33d6302cc8af56
wangshuai331 <wangshuai331@jd.com>
2020-06-29 20:16:36 +0800

    modify

---
+++ GALoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GALoginViewController.swift
+//  GALogin
+//
+//  Created by wangshuai331 on 2020/6/29.
+//
+
+import UIKit
+
+class GALoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
